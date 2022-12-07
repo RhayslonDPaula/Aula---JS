80685d7494743fac813e94e95414ed7b3cd68350
RHAYSLON MATHEUS DE PAULA CUNHA <37088883+RhayslonDPaula@users.noreply.github.com>
2022-12-07 20:33:14 -0300

    registration and login first class

---
+++ login-r/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import GlobalStyle from "./styles/global";
+
+
+const App = () => {
+  return (
+    <>
+      <div>App</div>;
+      <GlobalStyle />
+    </>
+  
+  );
+};
+
+export default App;
